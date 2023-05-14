9da63cf03b3352a153963b30d3f136e52a667df4
Gabriel Le_Rest <gabriellerest@gmail.com>
2023-05-14 16:25:43 +0200

    fin de l'exercice 04

--- app.js
+++ app.js
@@ -15,10 +15,14 @@ app.get("/api/movies/:id", database.getMoviesById);
 
 app.post("/api/movies", database.postMovie);
 
+app.put("/api/movies/:id", database.putMovie);
+
 app.get("/api/users", database.getUsers);
 app.get("/api/users/:id", database.getUsersById);
 
 app.post("/api/users", database.postUser);
+
+app.put("/api/users/:id", database.putUser);
 
 
 app.listen(port, (err) => {
--- database.js
+++ database.js
@@ -38,66 +38,115 @@ async function getMoviesById(req, res) {
 }
 
 const getUsers = async (req, res) => {
-  try {
-    const [users] = await pool.query("SELECT * FROM users");
-    if (users) {
-      return res.status(200).json(users);
-    } else {
-      return res.status(404).send("Not user found");
-    }
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send("Server error !!");
-  }
-}
+	try {
+		const [users] = await pool.query("SELECT * FROM users");
+		if (users) {
+			return res.status(200).json(users);
+		} else {
+			return res.status(404).send("Not user found");
+		}
+	} catch (err) {
+		console.error(err);
+		return res.status(500).send("Server error !!");
+	}
+};
 
 const getUsersById = async (req, res) => {
-  const id = parseInt(req.params.id);
-  try {
-    const usersId = await pool.query("SELECT * FROM users where id = ?", [id]);
-    if (usersId.length === 0) {
-      return res.status(404).send("Not users here");
-    } else {
-      return res.status(200).json(usersId[0]);
-    }
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send("Server lost, not your fault");
-  }
-}
+	const id = parseInt(req.params.id);
+	try {
+		const usersId = await pool.query("SELECT * FROM users where id = ?", [id]);
+		if (usersId.length === 0) {
+			return res.status(404).send("Not users here");
+		} else {
+			return res.status(200).json(usersId[0]);
+		}
+	} catch (err) {
+		console.error(err);
+		return res.status(500).send("Server lost, not your fault");
+	}
+};
 
 const postMovie = (req, res) => {
-  const { title, director, year, color, duration } = req.body;
+	const { title, director, year, color, duration } = req.body;
 
-  pool.query(
-    "INSERT INTO movies (title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
-    [title, director, year, color, duration],
-    (err, result) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send("Error saving the movie");
-      } else {
-        res.location(`/api/movies/${result.insertId}`).sendStatus(201);
-      }
-    }
-  );
+	pool.query(
+		"INSERT INTO movies (title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+		[title, director, year, color, duration],
+		(err, result) => {
+			if (err) {
+				console.error(err);
+				res.status(500).send("Error saving the movie");
+			} else {
+				res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+			}
+		}
+	);
 };
 
 const postUser = (req, res) => {
-  const { firstname, lastname, email, city, language } = req.body;
+	const { firstname, lastname, email, city, language } = req.body;
 
-  pool.query(
-    "INSERT INTO users (firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
-    [firstname, lastname, email, city, language],
-    (err, result) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send("Try again 😂🤣");
-      } else {
-        res.location(`/api/users/${result.insertId}`).sendStatus(201);
-      }
-    }
-  );
+	pool.query(
+		"INSERT INTO users (firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+		[firstname, lastname, email, city, language],
+		(err, result) => {
+			if (err) {
+				console.error(err);
+				res.status(500).send("Try again 😂🤣");
+			} else {
+				res.location(`/api/users/${result.insertId}`).sendStatus(201);
+			}
+		}
+	);
 };
 
-module.exports = { getMovies, getMoviesById, getUsers, getUsersById, postMovie, postUser };
+const putMovie = (req, res) => {
+	const id = parseInt(req.params.id);
+	const { title, director, year, color, duration } = req.body;
+
+	pool.query(
+		"UPDATE movies SET title = ?, director = ?, year = ?, color = ?, duration = ? WHERE id = ?",
+		[title, director, year, color, duration, id],
+		(err, [result]) => {
+			if (err) {
+				console.error(err);
+				res.status(500).send("Error editing the movie");
+			} else if (result.affectedRows === 0) {
+				res.status(404).send("Oups not found !!");
+			} else {
+				res.sendStatus(204);
+			}
+		}
+	);
+};
+
+function putUser(req, res) {
+	const id = parseInt(req.params.id);
+	const { firstname, lastname, email, city, language } = req.body;
+
+	pool.query(
+		"UPDATE users SET firstname = ?, lastname = ?, email = ?, city = ?, language = ? WHERE id = ?",
+		[firstname, lastname, email, city, language, id],
+		(err, [result]) => {
+			if (err) {
+				console.error(err);
+				res.status(500).send("ERRORRRRRRRRRRRRRRR");
+			} else if (result.affectedRows === 0) {
+				res.status(404).send("Unkown path");
+			} else {
+				res.sendStatus(204);
+			}
+		}
+	);
+};
+
+module.exports = {
+	getMovies,
+	getMoviesById,
+	getUsers,
+	getUsersById,
+	postMovie,
+	postUser,
+	putMovie,
+  putUser
+};
